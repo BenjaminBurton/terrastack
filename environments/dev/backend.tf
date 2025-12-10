d3af5dfe3e09a7acaba1f4475d374e5f8bb91106
BenjaminBurton <benjaminlburton@gmail.com>
2025-12-10 15:19:47 +0000

    style: format backend.tf with terraform fmt
    Automated formatting applied to ensure consistent code style
    - Adjusted comment spacing to Terraform standard

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terrastack-tfstate-1765377369"  # Replace this!
+    bucket         = "terrastack-tfstate-1765377369" # Replace this!
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terrastack-locks"
