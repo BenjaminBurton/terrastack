44ff235a1d68974b9b4a3252a1f5bd84fb07aee9
BenjaminBurton <benjaminlburton@gmail.com>
2025-12-11 00:07:44 +0000

    fix(rds): update PostgreSQL version from 16.3 to 16.6
    PostgreSQL 16.3 is not available in AWS RDS.
    Updated to 16.6 (latest available in 16.x series).
    
    Changes:
    - modules/rds/variables.tf: default version 16.6
    - environments/dev/variables.tf: default version 16.6
    
    Fixes error:
    Cannot find version 16.3 for postgres

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -63,7 +63,7 @@ variable "db_username" {
 variable "db_engine_version" {
   description = "PostgreSQL engine version"
   type        = string
-  default     = "16.3"
+  default     = "16.6"
 }
 
 variable "db_instance_class" {
